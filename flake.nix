7a0c007df96baf337711e467c2a37a6f0aa7dcbe
Ismael Bueno <ismaelwno@gmail.com>
2025-11-29 22:35:37 +0100

    chore: Add flake.nix

---
+++ flake.nix
@@ -0,0 +1,64 @@
+{
+  description = "Dev shell for physics-eugene";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  };
+
+  outputs =
+    { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+    in
+    {
+      devShells.${system}.default = pkgs.mkShell {
+        name = "physics-eugene";
+
+        buildInputs = with pkgs; [
+          clang
+          clang-tools
+          lld
+          libcxx
+          llvm
+
+          cmake
+          fmt
+          pkg-config
+          hwloc
+          perf
+          gnumake
+          python313
+
+          vulkan-loader
+          vulkan-headers
+          vulkan-tools
+          vulkan-memory-allocator
+          vulkan-validation-layers
+
+          spirv-tools
+          shaderc
+
+          glfw
+          wayland
+          wayland-protocols
+          wayland-scanner
+          libffi
+          libxkbcommon
+
+          fontconfig
+          expat
+        ];
+        shellHook = ''
+          export SHELL=${pkgs.zsh}/bin/zsh
+          export LD_LIBRARY_PATH=${pkgs.vulkan-loader}/lib:$LD_LIBRARY_PATH
+
+          export VK_LAYER_PATH=${pkgs.vulkan-validation-layers}/share/vulkan/explicit_layer.d
+
+          export CC=clang
+          export CXX=clang++
+          export CLANGD_FLAGS="$CLANGD_FLAGS --query-driver=/nix/store/*-clang-wrapper-*/bin/clang++"
+        '';
+      };
+    };
+}
